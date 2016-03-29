33f64e2592f775d95ac8bbfac6907c8a89cd6f37
Seryozha Poghosyan <seryozhapoghosyan@workfront.com>
2016-03-29 19:22:09 +0400

    Initial Commit

---
+++ NetworkingExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkingExamples
+//
+//  Created by Seryozha Poghosyan on 3/29/16.
+//  Copyright © 2016 Workfront. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NetworkingExamples/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetworkingExamples
+//
+//  Created by Seryozha Poghosyan on 3/29/16.
+//  Copyright © 2016 Workfront. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
